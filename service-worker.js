b2b6e224935ef1146b8730e231da068ace6a254b
dahaleambarish <75506731+dahaleambarish@users.noreply.github.com>
2020-12-04 23:19:50 +0000

    Deploying to gh-pages-mac from  @ a930f771e7a6c0566edd0205c1de0e7c6069f9db 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f5b6532f476b08be8ec0d8063d86086.js"
+  "/precache-manifest.8255cd2a60ccf03f92691770d82a8496.js"
 );
 
 self.addEventListener('message', (event) => {
